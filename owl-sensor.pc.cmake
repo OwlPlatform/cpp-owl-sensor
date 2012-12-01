82603bbfea43c9a5798a96bcbed497f6dedabddd
Ben Firner <bfirner@eden.rutgers.edu>
2012-12-01 13:20:05 -0500

    Adding cmake file (cmake didn't work before).

---
+++ owl-sensor.pc.cmake
@@ -0,0 +1,7 @@
+Name: owl-sensor
+Description: Owl platform sensor interface
+Requires: owl
+Version: @LibOwlSensor_VERSION_MAJOR@
+Libs: -L@LIB_DESTINATION@ -lowl-sensor
+Cflags: -I@INCLUDE_INSTALL_DIR@
+
